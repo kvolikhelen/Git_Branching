374b1c5b09891e9f1434b1eebc30f13ab2b49cc6
Husna <106981552+husnaozyildirim@users.noreply.github.com>
2022-08-30 12:37:26 -0400

    Update HelloHusna.java

--- src/HusnaWork/HelloHusna.java
+++ src/HusnaWork/HelloHusna.java
@@ -4,5 +4,7 @@ public class HelloHusna {
     public static void main(String[] args) {
         System.out.println("Husna Hello)))");
         System.out.println("How are you doing, girl?");
+        
+        System.out.println("I need more coffe");
     }
 }
