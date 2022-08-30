5bed72f0ab0b715abf64f39e6b2d94992d053a33
Cem Celik <cemocelik35@gmail.com>
2022-08-30 12:27:39 -0400

    Hello tester class created.

---
+++ src/test/HelloTesters.java
@@ -0,0 +1,8 @@
+package test;
+
+public class HelloTesters {
+    public static void main(String[] args) {
+        System.out.println("Hello hello!");
+        System.out.println("do you see y code?");
+    }
+}
